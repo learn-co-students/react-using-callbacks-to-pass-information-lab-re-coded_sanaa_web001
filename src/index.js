8f152314f00346c3c4c8738513298589d1f41b91
Ahmed Aleryani <aalavyani91@yahoo.com>
2020-09-02 22:35:33 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import learnSymbol from './data.js'
 import Matrix from './Matrix.js'
-
+ 
 ReactDOM.render(<Matrix values={learnSymbol} />, document.getElementById('root'));
