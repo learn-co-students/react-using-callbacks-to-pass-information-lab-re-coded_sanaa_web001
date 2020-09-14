695629d1eb03230e7a359faa29032c624f7f0d09
OsamahGharad <osamah.gharred@gmail.com>
2020-09-14 21:13:45 +0300

    Done.

--- src/Matrix.js
+++ src/Matrix.js
@@ -7,7 +7,18 @@ export default class Matrix extends Component {
 
   constructor() {
     super()
+    this.state = {
+      color:  '#FFFF'
+    }
   }
+
+  setSelectedColor = (newColor) => {
+    this.setState({
+      color : newColor
+    })
+  }
+
+  getSelectedColor = () => (this.state.color)
 
   genRow = (vals) => (
     vals.map((val, idx) => <Cell key={idx} color={val} />)
@@ -17,18 +28,19 @@ export default class Matrix extends Component {
     this.props.values.map((rowVals, idx) => <div key={idx} className="row">{this.genRow(rowVals)}</div>)
   )
 
+
   render() {
     return (
       <div id="app">
-        <ColorSelector />
+        <ColorSelector setSelectedColor={this.setSelectedColor}/>
         <div id="matrix">
           {this.genMatrix()}
         </div>
       </div>
     )
   }
-}
 
+}
 Matrix.defaultProps = {
   values: learnSymbol
 }
