5b5f33748d2b991ca2aacaa41e47d81b48003938
Tahany Alswoidy <tahanyalswoidy@gmail.com>
2020-09-22 12:06:59 +0000

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-
+ 
 export default class Cell extends Component {
-  
+ 
   constructor(props) {
     super(props)
     this.state = {
       color: this.props.color
     }
   }
-  
+  genRow = (vals) => (
+    vals.map((val, idx) => <Cell key={idx} color={val} selectedColor={this.state.selectedColor} />)
+  )
+  handleClick = () => {
+    this.setState({
+      color: this.props.selectedColor
+    })
+  }
+ 
   render() {
     return (
-      <div className="cell" style={{backgroundColor: this.state.color}}>
+      <div onClick={this.handleClick} className="cell"
+           style={{backgroundColor: this.state.color}}
+      >
       </div>
     )
   }
-  
+ 
 }
